92d079a7bcb329ba63c3d201a2ea14f53b959d4a
dkja <dkja@users.noreply.github.com>
2025-07-14 17:35:49 +0200

    Build native amd64 app by graalvm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     application
+    alias(libs.plugins.graalvm.buildtools.native)
 }
 
 repositories {
@@ -9,22 +10,25 @@ repositories {
 
 dependencies {
     implementation(libs.ktor.server.cio)
+    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.call.logging)
     implementation(libs.ktor.server.metrics.micrometer)
     implementation(libs.ktor.server.html.builder)
 
     implementation(libs.logback.classic)
     implementation(libs.micrometer.registry.prometheus)
-    implementation("io.ktor:ktor-server-core:3.2.1")
-    implementation("io.ktor:ktor-server-core:3.2.1")
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
 }
 
 application {
     mainClass = "org.example.AppKt"
+}
+
+graalvmNative {
+    binaries.all {
+        buildArgs.addAll(
+            "-H:+InstallExitHandlers",
+            "-H:+ReportUnsupportedElementsAtRuntime",
+            "-H:+ReportExceptionStackTraces"
+        )
+    }
 }
