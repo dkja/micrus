7f051b8a2c39cec3ca6e6b3057bfaeaa34055d35
dkja <dkja@users.noreply.github.com>
2025-07-15 21:40:47 +0200

    ktor server core dependency update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,14 +9,13 @@ repositories {
 
 dependencies {
     implementation(libs.ktor.server.cio)
+    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.call.logging)
     implementation(libs.ktor.server.metrics.micrometer)
     implementation(libs.ktor.server.html.builder)
 
     implementation(libs.logback.classic)
     implementation(libs.micrometer.registry.prometheus)
-    implementation("io.ktor:ktor-server-core:3.2.1")
-    implementation("io.ktor:ktor-server-core:3.2.1")
 }
 
 java {
